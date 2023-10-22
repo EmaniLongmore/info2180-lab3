ddd0d487c287002f264047f4e931e03d3905a222
Emani Longmore <emanilongmore2@gmail.com>
2023-10-22 16:18:02 -0400

    Edited tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -57,5 +57,58 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
+  document.addEventListener('DOMContentLoaded', function() {
+    const squares = document.querySelectorAll('#board div');
+    const status = document.getElementById('status');
+  
+    let currentPlayer = 'X';
+    let gameBoard = ['', '', '', '', '', '', '', '', ''];
+  
+    squares.forEach((square, index) => {
+      square.classList.add('square'); // Add the 'square' class to each div element
+  
+      square.addEventListener('mouseenter', function() {
+        square.classList.add('hover'); // Add the 'hover' class when the mouse enters the square
+      });
+  
+      square.addEventListener('mouseleave', function() {
+        square.classList.remove('hover'); // Remove the 'hover' class when the mouse leaves the square
+      });
+  
+      square.addEventListener('click', function() {
+        if (gameBoard[index] === '' && !checkWinner()) {
+          gameBoard[index] = currentPlayer;
+          square.textContent = currentPlayer;
+          square.classList.add(currentPlayer);
+  
+          if (checkWinner()) {
+            status.textContent = `Congratulations! ${currentPlayer} is the Winner!`;
+            status.classList.add('you-won');
+          } else {
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            status.textContent = `It's ${currentPlayer}'s turn.`;
+            status.classList.remove('you-won');
+          }
+        }
+      });
+    });
+  
+    function checkWinner() {
+      const winningCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6] // Diagonals
+      ];
+  
+      for (const combination of winningCombinations) {
+        const [a, b, c] = combination;
+        if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+          return true;
+        }
+      }
+  
+      return false;
+    }
+  });
   
 
