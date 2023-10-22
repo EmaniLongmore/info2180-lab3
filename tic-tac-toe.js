06de1b2f7d1ac6aa880163ce6478099d978226aa
Emani Longmore <emanilongmore2@gmail.com>
2023-10-22 16:07:36 -0400

    Edited tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const squares = document.querySelectorAll('#board div');
+  
+    squares.forEach(square => {
+      square.classList.add('square'); // Add the 'square' class to each div element
+    });
+  });
+  
+
+// Initialize an empty array to keep track of the game state
+var gameState = [];
+
 // Wait for the DOM content to be fully loaded
 document.addEventListener("DOMContentLoaded", function() {
     // Get all the squares using the square class
     var squares = document.querySelectorAll('.square');
 
-    // Loop through each square and add appropriate classes
-    squares.forEach(function(square) {
-        // Add the 'empty' class to indicate that the square is empty
-        square.className = 'square empty'
-        // You can add more classes if needed, such as 'x' or 'o' for player moves
+    // Add click event listener to each square
+    squares.forEach(function(square, index) {
+        // Initialize the game state with null values (indicating empty squares)
+        gameState[index] = null;
+
+        square.addEventListener('click', function() {
+            // Check if the square is empty
+            if (gameState[index] === null) {
+                // Determine current player (X or O)
+                var currentPlayer = (gameState.filter(Boolean).length % 2 === 0) ? 'X' : 'O';
+
+                // Update the game state and display X or O in the square
+                gameState[index] = currentPlayer;
+                square.textContent = currentPlayer;
+
+                // Add the class to style the square with appropriate color
+                square.classList.add(currentPlayer);
+
+                // You can now use the gameState array to check for a win or a draw
+            }
+        });
     });
 });
